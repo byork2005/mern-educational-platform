88c042060237106d0b27483a4e67fde3ad01bdff
Purewal <SP056756@cerner.net>
2018-03-02 12:30:25 -0600

    added student stuff

--- client/src/pages/StudentHome/studentHome.js
+++ client/src/pages/StudentHome/studentHome.js
@@ -5,15 +5,69 @@ import DeleteBtn from "../../components/DeleteBtn";
 import "./studentHome.css";
 
 const studentContainer = () =>
-  <Container fluid>
-    <Row>
-      <Col size="md-12">
-<ul>
-  <li>Psychology<DeleteBtn/></li>
-  <li>History<DeleteBtn/></li>
-</ul>
-      </Col>
-    </Row>
-  </Container>;
+<Container fluid>
+<div className="well">
+      <div className="media">
+      	<a className="pull-left" href="#">
+    		<img className="media-object" src="http://via.placeholder.com/150x150"/>
+  		</a>
+  		<div className="media-body">
+    		<h4 className="media-heading">History 101</h4>
+          <p className="text-right">Professor Spencer</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
+Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
+dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
+Aliquam in felis sit amet augue.</p>
+          <ul className="list-inline list-unstyled">
+  			<li><span><i className="glyphicon glyphicon-calendar"></i> 2 days, 8 hours </span></li>
+            <li>|</li>
+            <span><i className="glyphicon glyphicon-comment"></i> 2 comments</span>
+            <li>|</li>
+            <li>
+               <span className="glyphicon glyphicon-star"></span>
+                        <span className="glyphicon glyphicon-star"></span>
+                        <span className="glyphicon glyphicon-star"></span>
+                        <span className="glyphicon glyphicon-star"></span>
+                        <span className="glyphicon glyphicon-star-empty"></span>
+            </li>
+            <li>|</li>
+			</ul>
+       </div>
+    </div>
+  </div>
+   <div className="well">
+      <div className="media">
+  		<a className="pull-left" href="#">
+    		<img className="media-object" src="http://via.placeholder.com/150x150"/>
+  		</a>
+  		<div className="media-body">
+    		<h4 className="media-heading">Math 101</h4>
+          <p className="text-right">Ms. Teacher</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
+Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
+dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
+Aliquam in felis sit amet augue.</p>
+          <ul className="list-inline list-unstyled">
+  			<li><span><i className="glyphicon glyphicon-calendar"></i> 2 days, 8 hours </span></li>
+            <li>|</li>
+            <span><i className="glyphicon glyphicon-comment"></i> 2 comments</span>
+            <li>|</li>
+            <li>
+               <span className="glyphicon glyphicon-star"></span>
+                        <span className="glyphicon glyphicon-star"></span>
+                        <span className="glyphicon glyphicon-star"></span>
+                        <span className="glyphicon glyphicon-star"></span>
+                        <span className="glyphicon glyphicon-star-empty"></span>
+            </li>
+            <li>|</li>
+			</ul>
+       </div>
+    </div>
+  </div>
 
+
+
+
+
+</Container>
 export default studentContainer;
