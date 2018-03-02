fb9ba0c724ce48aec4faa78211e315cec5b6bfb5
Purewal <SP056756@cerner.net>
2018-03-02 11:06:21 -0600

    removed some book stuff

--- client/src/pages/StudentHome/studentHome.js
+++ client/src/pages/StudentHome/studentHome.js
@@ -2,14 +2,15 @@ import React from "react";
 import { Col, Row, Container } from "../../components/Grid";
 import Jumbotron from "../../components/Jumbotron";
 import DeleteBtn from "../../components/DeleteBtn";
+import "./studentHome.css";
 
 const studentContainer = () =>
   <Container fluid>
     <Row>
       <Col size="md-12">
 <ul>
-  <li>Psychology.........<DeleteBtn/></li>
-  <li>History............<DeleteBtn/></li>
+  <li>Psychology<DeleteBtn/></li>
+  <li>History<DeleteBtn/></li>
 </ul>
       </Col>
     </Row>
