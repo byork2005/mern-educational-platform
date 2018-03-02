34a3830f08b5fe67f1f4573bb75d8c6c1ae67a60
Brandon York <30988963+byork2005@users.noreply.github.com>
2018-03-01 20:20:11 -0600

    basic functionality

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Signup from './components/sign-up'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <Signup/>
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
