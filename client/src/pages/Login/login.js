7d66554cd20b2459458b65edddc0bc24496eabe3
Purewal <SP056756@cerner.net>
2018-03-02 14:36:21 -0600

    fixed login page, more professional

--- client/src/pages/Login/login.js
+++ client/src/pages/Login/login.js
@@ -1,37 +1,41 @@
 import React from "react";
-import { Col, Row, Container } from "../../components/Grid";
-import Jumbotron from "../../components/Jumbotron";
+// import { Col, Row, Container } from "../../components/Grid";
+// import Jumbotron from "../../components/Jumbotron";
 // import { Input, TextArea, FormBtn } from "../../components/Form";
-import { LoginInput, LoginBtn } from "../../components/LoginStuff";
+// import { LoginInput, LoginBtn } from "../../components/LoginStuff";
 import "./login.css";
 
 
 const Login = () =>
-  <Container fluid>
-    <Row>
-      <Col size="md-12">
-
-          <h1 className = "text-center">Log in here</h1>
-          <LoginInput
-                // value={this.state.username}
-                // onChange={this.handleInputChange}
-                name="username"
-                placeholder="username (required)"
-              />
-                        <LoginInput
-                // value={this.state.username}
-                // onChange={this.handleInputChange}
-                name="password"
-                placeholder="password (required)"
-              />
-              <LoginBtn
-                // disabled={!(this.state.author && this.state.title)}
-                // onClick={this.handleFormSubmit}
-              >
-                Login
-              </LoginBtn>
-      </Col>
-    </Row>
-  </Container>;
+<div class="container">
+  <div class="row">
+    <div class="Absolute-Center is-Responsive">
+      <div id="logo-container"></div>
+      <div class="col-sm-12 col-md-10 col-md-offset-1">
+        <form action="" id="loginForm">
+          <div class="form-group input-group">
+            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+            <input class="form-control" type="text" name='username' placeholder="username"/>          
+          </div>
+          <div class="form-group input-group">
+            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+            <input class="form-control" type="password" name='password' placeholder="password"/>     
+          </div>
+          <div class="checkbox">
+            <label>
+              <input type="checkbox"/> I agree to the <a href="asdasd">Terms and Conditions</a>
+            </label>
+          </div>
+          <div class="form-group">
+            <button type="button" class="btn btn-def btn-block">Login</button>
+          </div>
+          <div class="form-group text-center">
+            <a href="asdasd">Forgot Password</a>&nbsp;|&nbsp;<a href="asdads">Support</a>
+          </div>
+        </form>        
+      </div>  
+    </div>    
+  </div>
+</div>
 
 export default Login;
