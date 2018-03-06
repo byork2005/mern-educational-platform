47a7ed6e637b1c7e4644bac8d394b6c96d431f93
Suhail Purewal <suhail.purewal@gmail.com>
2018-03-05 19:45:40 -0600

    cleaning up more

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,36 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import NoMatch from "./pages/NoMatch";
-import StudentHome from "./pages/StudentHome";
-import TeacherHome from "./pages/TeacherHome";
-import TeacherClass from "./pages/TeacherClass";
-import Login from "./pages/Login";
-import Nav from "./components/Nav";
+// import StudentHome from "./pages/StudentHome";
+// import TeacherHome from "./pages/TeacherHome";
+// import TeacherClass from "./pages/TeacherClass";
+// import Login from "./pages/Login";
+import {Nav, NavItem} from "react-bootstrap";
+
+
+function handleSelect(selectedKey) {
+  alert(`selected ${selectedKey}`);
+}
 
 const App = () =>
   <Router>
     <div>
-      <Nav />
+    <Nav bsStyle="pills" activeKey={1} onSelect={handleSelect}>
+    <NavItem eventKey={1} href="/home">
+      NavItem 1 content
+    </NavItem>
+    <NavItem eventKey={2} title="Item">
+      NavItem 2 content
+    </NavItem>
+    <NavItem eventKey={3} disabled>
+      NavItem 3 content
+    </NavItem>
+  </Nav>
       <Switch>
-        <Route exact path="/" component={Login} />
+        {/* <Route exact path="/" component={Login} />
         <Route exact path="/studenthome" component={StudentHome} />
         <Route exact path="/teacherhome" component={TeacherHome} />
-        <Route exact path="/teacherclass" component={TeacherClass} />
-        <Route component={NoMatch} />
+        <Route exact path="/teacherclass" component={TeacherClass} /> */}
+        {/* <Route component={NoMatch} /> */}
       </Switch>
     </div>
   </Router>;
