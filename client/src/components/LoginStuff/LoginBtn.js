43bc4c21f7d6668cca1f4dbaf8e6f09910a1efff
Suhail Purewal <suhail.purewal@gmail.com>
2018-03-05 19:24:36 -0600

    purgggeee

--- client/src/components/LoginStuff/LoginBtn.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./LoginInput.css";
-
-export const LoginBtn = props =>
-  <button {...props} className="login-btn btn btn-success center-block">
-    {props.children}
-  </button>;
