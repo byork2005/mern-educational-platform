f839b71596e7dc03e494c2ac5ffae3623a92c6ce
unknown <suhail.purewal@gmail.com>
2018-03-07 13:17:31 -0600

    added student home components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import TopNav from "./components/TopNav";
-// import StudentHome from "./pages/StudentHome";
+import StudentHome from "./pages/StudentHome";
 import TeacherClassPage from "./pages/TeacherClassPage";
 // import TeacherClass from "./pages/TeacherClass";
 // import Login from "./pages/Login";
@@ -17,9 +17,9 @@ const App = () =>
     <div>
       <TopNav />
       <Switch>
-        {/* <Route exact path="/" component={Login} />
+        {/* <Route exact path="/" component={Login} /> */}
         <Route exact path="/studenthome" component={StudentHome} />
-        <Route exact path="/teacherhome" component={TeacherHome} /> */}
+        {/* <Route exact path="/teacherhome" component={TeacherHome} /> */}
         <Route exact path="/teacherclass" component={TeacherClassPage} />
         {/* <Route component={NoMatch} /> */}
       </Switch>
