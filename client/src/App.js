d2f7668d92df33dabbbd9dd372a7104847a66c4c
Harry Spencer <haspencer4@gmail.com>
2018-03-12 20:09:11 -0500

    route changes

--- client/src/App.js
+++ client/src/App.js
@@ -65,8 +65,8 @@ class App extends Component {
           <Route path="/teacherhome" component={TeacherHomePage} />
           <Route path="/teacherclass" component={TeacherClassPage} />
           <Route path="/studentclass" component={StudentClassPage} />
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={Signup} />
+          <Route path="/signup" render={ () => (<Signup Signup={this.signup}/>)}/>
+          <Route path="/login" render={ () => (<Login updateUser={this.updateUser}/>)} />>
 
           {/* <Route component={NoMatch} /> */}
         </Switch>
